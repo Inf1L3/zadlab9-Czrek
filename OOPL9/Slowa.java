ecd22611261019fbbfacaa7c9f062b337c7c0aa1
Czrek <37151199+Czrek@users.noreply.github.com>
2018-05-16 08:58:45 +0200

    Add files via upload

---
+++ OOPL9/Slowa.java
@@ -0,0 +1,35 @@
+package zadanie9;
+ 
+import java.util.HashMap;
+
+public class Slowa {
+     public void zad(){}
+     
+    
+
+ public void slowko(HashMap<String, String> slownik) {
+
+        slownik.put("byc", "be");
+        slownik.put("otrzymac", "get");
+        slownik.put("dzien", "day");
+        slownik.put("umowa", "deal");
+        slownik.put("obiad", "dinner");
+        slownik.put("obowiazek", "duty");
+        slownik.put("miec", "have");
+        slownik.put("sloik", "jar");
+        slownik.put("praca", "job");
+        slownik.put("dodawac", "add");
+        slownik.put("wiek", "age");
+        slownik.put("tata", "father");
+        slownik.put("mama", "mother");
+        slownik.put("syn", "son");
+        slownik.put("wujek", "uncle");
+        slownik.put("samochod", "car");
+        slownik.put("woda", "water");
+        slownik.put("stol", "table");
+        slownik.put("telefon", "mobile phone");
+        slownik.put("buty", "shoes");
+
+    }
+    
+ }
---
+++ OOPL9/Tlumacz.java
@@ -0,0 +1,33 @@
+package zadanie9;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+
+public class Tlumacz {
+
+    public static void main(String[] args) {
+        
+        HashMap<String, String> slownik = new HashMap<>();
+        Scanner input=new Scanner(System.in);
+        Slowa slowa1=new Slowa();
+        slowa1.slowko(slownik);
+        String a=" ";
+        
+        while(!a.equals("koniec"))
+           {
+             
+              System.out.println("Podaj słowo ktore chcesz przetlumaczyc:");
+              a = input.nextLine();
+              
+               
+               
+              if(!slownik.containsKey(a))
+              {
+                  System.out.println("Takiego slowa nie ma w slowniku ");
+             
+              }
+              else
+                    System.out.println(slownik.get(a));
+               System.out.println("Jesli chcesz zakonczyc napisz koniec");
+           }}}
